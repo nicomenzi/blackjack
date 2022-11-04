bfef676d1e743ab14493c5926e0b4086c6b29788
tobiasbertschi <gotteron89tb@gmail.com>
2022-11-04 15:15:05 +0100

    Create File Cards

---
+++ src/Cards.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {useState} from "react";
+
+export default function Cards() {
+
+    /*const [deck, setDeck] = useState("");
+    const [cards, setCard] = useState([])
+
+    const btHandler = () =>
+        fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')*/
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <p>Blackjack</p>
+
+                {/*<div>
+                    <button>draw next card</button>
+                </div>*/}
+
+            </header>
+        </div>
+    );
+}
