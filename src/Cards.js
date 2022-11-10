6260920db875e7e8e9210680fbc0ceef3f94724d
tobiasbertschi <gotteron89tb@gmail.com>
2022-11-10 11:41:48 +0100

    array playercard & dealercard hinzugefügt & function bearbeitet

--- src/Cards.js
+++ src/Cards.js
@@ -4,7 +4,8 @@ import {useEffect, useState} from 'react';
 export default function Cards() {
     const [deck, setDeck] = useState([]);
     const [deckID, setDeckID] = useState();
-    const [card, setCard] = useState([]);
+    const [dealercard, setDealercard] = useState([])
+    const [playercard, setPlayercard] = useState([])
 
     function getDeck() {
         fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
@@ -17,10 +18,10 @@ export default function Cards() {
     }
 
 
-    function getCard() {
+    function getCard(setCard, currentCards) {
         fetch('https://deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=1')
             .then(response => response.json())
-            .then(data => setCard(card => [...card, data.cards[0]]))
+            .then(data => setCard(currentCards => [...currentCards, data.cards[0]]))
             // .then(data => setCard(data.cards[0]))            //so wird die Karte jeweils überschrieben
             .catch((error) => {
                 console.error('Error:', error);
@@ -28,21 +29,27 @@ export default function Cards() {
     }
 
     function clearCards() {
-        setCard([]);
+        setPlayercard([]);
+        setDealercard([]);
     }
 
     useEffect(() => {
         getDeck()
     }, []);               //Ruft beim starten der Seite die function getDeck () auf
     useEffect(() => {
-        console.log(card)
-    }, [card]);
+        console.log(playercard)
+        console.log(dealercard)
+    }, [playercard]);
 
     return (<div className="App">
             <header className="App-header">
-                <button onClick={getCard}>get Card</button>
-                {card.map((c) => <img src={c.image}></img>)}
                 {/*{card && <img src={card.image}></img>}      dies hat nur funktioniert als card oben noch kein Array war*/}
+                <button onClick={(e) => getCard(setPlayercard, playercard)}>Player</button>
+                {playercard.map((c) => <img src={c.image}></img>)}
+
+                <button onClick={(e) => getCard(setDealercard, dealercard)}>Dealer</button>
+                {dealercard.map((c) => <img src={c.image}></img>)}
+
                 <button onClick={clearCards}>clear Cards</button>
             </header>
         </div>
