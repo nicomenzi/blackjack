0f30b4cbd92cf794304ac2c11708a5f6e4b5695c
nico <nicomenzii@gmail.com>
2022-11-11 14:34:44 +0100

    finish game method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Cards from "./Cards";
 
 function App() {
   return (
@@ -20,6 +21,35 @@ function App() {
       </header>
     </div>
   );
+}
+
+function finish(){
+
+  let playercard = Cards.playercard;
+  let dealercard = Cards.dealercard;
+
+  while (Cards.calculate(dealercard) < 17) {
+    Cards.getCard(Cards.setDealercard, dealercard);
+  }
+  let dealer = Cards.calculate(dealercard);
+  let player = Cards.calculate(playercard);
+
+    if (player > 21) {
+      alert("You lost!");
+    }
+    else if (dealer > 21) {
+      alert("You won!");
+    }
+    else if (player > dealer) {
+      alert("You won!");
+    }
+    else if (player < dealer) {
+      alert("You lost!");
+    }
+    else {
+      alert("Draw!");
+    }
+
 }
 
 export default App;
