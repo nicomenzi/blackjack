0b990a558769ba2cc72c9073d4381e03a130aa48
tobiasbertschi <gotteron89tb@gmail.com>
2022-12-20 20:43:24 +0100

    player works now

--- src/Cards.js
+++ src/Cards.js
@@ -12,8 +12,6 @@ export default function Cards() {
     const [dealercard, setDealercard] = useState([])
     const [playercard, setPlayercard] = useState([])
 
-
-    //Ruft beim starten der Seite die function getDeck () auf
     useEffect(() => {
         fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
             .then(response => response.json())
@@ -33,7 +31,12 @@ export default function Cards() {
             .catch((error) => {
                 console.error('Error:', error);
             });
+    }
 
+    const clearCards = () => {
+        setPlayercard([]);
+        setDealercard([]);
+        window.location.reload(false);
     }
 
     const calculate = (cards, isPlayer) => {
@@ -43,50 +46,47 @@ export default function Cards() {
         //console.log("not for" + cards);
         for (const sum of cards) {
             const sumValue = sum.value;
-            //console.log("for")
             if (VALUE_10.includes(sumValue)) {
                 result += 10;
-                //console.log(result);
-            } else if (sumValue == "ACE" && result <= 10) {
+            } else if (sumValue === "ACE" && result <= 10) {
                 result += 11;
-                //console.log(result);
-            } else if (sumValue == "ACE" && result > 10) {
+            } else if (sumValue === "ACE" && result > 10) {
                 result += 1;
-                //console.log(result);
             } else {
                 result += parseInt(sumValue);
-                console.log(result);
             }
         }
 
-        console.log(result)
-        isPlayer && setPlayerValue(result) || setDealerValue(result);
+        console.log(result);
+        isPlayer ? setPlayerValue(result) : setDealerValue(result);
+
     }
 
-    const finish = (player, dealer) => {
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-        //geht so nicht, da Value zu langsam geupdated wird
-        /*while (dealer < 17) {
+    const finish = async (player) => {
+
+        //so gaht das nöd
+        /*while (dealerValue < 17) {
             getCard(setDealercard, dealercard);
+            await delay(500);
+            calculate(dealercard, false);
         }*/
 
-        if (player > dealer) {
+        if (player > dealerValue) {
             alert("You won!");
-        } else if (player < dealer) {
+            window.location.reload(false);
+        } else if (player < dealerValue) {
             alert("You lost!");
+            window.location.reload(false);
         } else {
             alert("Draw!");
+            window.location.reload(false);
         }
     }
 
-    const clearCards = () => {
-        setPlayercard([]);
-        setDealercard([]);
-        window.location.reload(false);
-    }
-
     useEffect(() => {
-        if (deckID != undefined) {
+        if (deckID !== undefined) {
             getCard(setPlayercard, playercard);
             getCard(setPlayercard, playercard);
             getCard(setDealercard, dealercard);
@@ -94,65 +94,70 @@ export default function Cards() {
     }, [deckID]);
 
     useEffect(() => {
-        console.log(playercard)
-
-        if (playercard != undefined) {
+        if (playercard !== undefined) {
             calculate(playercard, true);
-            console.log(playerValue)
-            const valueLost = playerValue > 21
-            if (valueLost) {
-                //geht so nocht nicht ganz, da Value zu langsam geupdated wird
-                alert("You lost")
-                window.location.reload(false);
-            }
         }
     }, [playercard]);     //wird erst gemacht, wenn playercard gemacht/abgefüllt wurde
 
     useEffect(() => {
-        console.log(dealercard)
-        dealercard != undefined && calculate(dealercard, false)
-    }, [dealercard]);     //wird erst gemacht, wenn playercard gemacht/abgefüllt wurde
+        if (dealercard !== undefined) {
+            calculate(dealercard, false);
+        }
+    }, [dealercard]);
 
+    useEffect(() => {
+        console.log(playerValue);
+        const valueLost = playerValue > 21;
+        if (valueLost) {
+            //geht so nocht nicht ganz, da Value zu langsam geupdated wird
+            setTimeout(() => {
+                alert("You lost")
+                window.location.reload(false);
+            }, 100);
+        }
+    }, [playerValue]);
 
+//Ruft beim starten der Seite die function getDeck () auf
     return (<div className="App">
-            <header className="App-header">
-                {/*{card && <img src={card.image}></img>}      dies hat nur funktioniert als card oben noch kein Array war*/}
-                <Grid container rowSpacing={3} alignItems="flex-start" justifyContent="center">
-                    <Grid item>
-                        <Button variant="contained" onClick={(e) => getCard(setPlayercard, playercard)}>hit</Button>
-                    </Grid>
-                    <Grid container spacing={5} alignItems="center" justifyContent="center">
-                        {playercard.map((c) =>
-                            <Grid item>
-                                <img src={c.image}></img>
-                                <p>{c.value}</p>
-                            </Grid>)}
-                    </Grid>
-                    <Grid>
-                        {playerValue}
-                    </Grid>
 
-                    <Grid item>
-                        <Button variant="contained"
-                                onClick={(e) => finish(playerValue, dealerValue)}>stand</Button>
-                    </Grid>
-                    <Grid container spacing={5} alignItems="center" justifyContent="center">
-                        {dealercard.map((c) => <Grid item>
-                            <img src={c.image}></img>
-                            <p>{c.value}</p>
-                        </Grid>)}
-                    </Grid>
-                    <Grid>
-                        {dealerValue}
-                    </Grid>
+        <header className="App-header">
+            <container rowSpacing={3} alignItems="flex-start" justifyContent="center">
 
-                    <Grid item>
-                        <Button variant="contained" onClick={clearCards}>Restart</Button>
-                    </Grid>
-
+                <Grid item>
+                    <Button variant="contained" onClick={(e) => getCard(setPlayercard, playercard)}>hit</Button>
                 </Grid>
-            </header>
-        </div>
 
-    )
+                <Grid container spacing={5} alignItems="center" justifyContent="center">
+                    {playercard.map((c) => <Grid item>
+                        <img src={c.image} alt="PlayerCard"></img>
+                        <p>{c.value}</p>
+                    </Grid>)}
+                </Grid>
+
+                <Grid>
+                    {playerValue}
+                </Grid>
+
+                <Grid item>
+                    <Button variant="contained"
+                            onClick={(e) => finish(playerValue)}>stand</Button>
+                </Grid>
+                <Grid container spacing={5} alignItems="center" justifyContent="center">
+                    {dealercard.map((c) => <Grid item>
+                        <img src={c.image} alt="DealerCard"></img>
+                        <p>{c.value}</p>
+                    </Grid>)}
+                </Grid>
+                <Grid>
+                    {dealerValue}
+                </Grid>
+
+                <Grid item>
+                    <Button variant="contained" onClick={clearCards}>Restart</Button>
+                </Grid>
+            </container>
+
+        </header>
+
+    </div>)
 }
